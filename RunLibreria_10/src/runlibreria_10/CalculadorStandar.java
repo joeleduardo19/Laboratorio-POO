2c4122e944861b82dfb6ccbb212ab33f60f60fe3
joeleduardo19 <joelvargasrivera5@gmail.com>
2022-11-19 00:43:06 -0600

    Run libreria practica10

---
+++ RunLibreria_10/src/runlibreria_10/CalculadorStandar.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runlibreria_10;
+import Lib.Calculadora;
+/**
+ *
+ * @author Dell
+ */
+public class CalculadorStandar extends Calculadora {
+
+    @Override
+    public double suma(double a, double b) {
+        return a + b;
+    }
+
+    @Override
+    public double multiplicacion(double a, double b) {
+        return a * b;
+    }
+    
+}
---
+++ RunLibreria_10/src/runlibreria_10/RunLibreria_10.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package runlibreria_10;
+
+/**
+ *
+ * @author Dell
+ */
+public class RunLibreria_10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CalculadorStandar casio = new CalculadorStandar();
+        System.out.println(casio.suma(10, 20));
+        System.out.println(casio.resta(45, 15));
+        System.out.println(casio.division(5, 5));
+        System.out.println(casio.multiplicacion(4, 8));
+    }
+    
+}
